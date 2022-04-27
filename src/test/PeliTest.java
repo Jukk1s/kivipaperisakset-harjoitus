e77c041a4c44120177303a621571e286bafc4703
Jukkis <75067146+Jukk1s@users.noreply.github.com>
2022-04-27 21:40:36 +0300

    Add files via upload

---
+++ src/test/PeliTest.java
@@ -0,0 +1,85 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import kivipaperisakset.Pelaaja;
+import kivipaperisakset.Peli;
+
+class PeliTest {
+	
+	Peli peli;
+	Pelaaja pelaaja1;
+	Pelaaja pelaaja2;
+	
+
+	@Test
+	public void testTarkistaVoittaja() {
+		pelaaja1 = new Pelaaja();
+        pelaaja2 = new Pelaaja();
+        peli = new Peli(pelaaja1, pelaaja2);
+        
+		String failText = "Voittaja väärin";
+		
+		pelaaja1.setValinta("KIVI");
+		pelaaja2.setValinta("SAKSET");
+		assertEquals(pelaaja1, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("SAKSET");
+		pelaaja2.setValinta("PAPERI");
+		assertEquals(pelaaja1, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("PAPERI");
+		pelaaja2.setValinta("KIVI");
+		assertEquals(pelaaja1, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("SAKSET");
+		pelaaja2.setValinta("KIVI");
+		assertEquals(pelaaja2, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("PAPERI");
+		pelaaja2.setValinta("SAKSET");
+		assertEquals(pelaaja2, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("KIVI");
+		pelaaja2.setValinta("PAPERI");
+		assertEquals(pelaaja2, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("KIVI");
+		pelaaja2.setValinta("KIVI");
+		assertEquals(null, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("PAPERI");
+		pelaaja2.setValinta("PAPERI");
+		assertEquals(null, peli.tarkistaVoittaja(), failText);
+		
+		pelaaja1.setValinta("SAKSET");
+		pelaaja2.setValinta("SAKSET");
+		assertEquals(null, peli.tarkistaVoittaja(), failText);
+	}
+	
+	@Test
+	public void testArvoPelaajienValinnat() {
+		pelaaja1 = new Pelaaja();
+        pelaaja2 = new Pelaaja();
+        peli = new Peli(pelaaja1, pelaaja2);
+        boolean virhe = false;
+        
+		String failText = "Metodi palauttaa virheellisen taulukon";
+		
+		String taulukko[] = peli.arvoPelaajienValinnat();
+		
+		if (!(taulukko[0].equals("KIVI") || taulukko[0].equals("PAPERI") || taulukko[0].equals("SAKSET")
+				|| taulukko[1].equals("KIVI") || taulukko[1].equals("PAPERI") || taulukko[1].equals("SAKSET"))) {
+			virhe = true;
+		}
+		assertEquals(false,virhe,failText);
+		
+	}
+
+}
